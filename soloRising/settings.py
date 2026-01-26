153ed2fd6cc989085f466ab160ea1d416904c698
D-FAKE <shaikhzaid23154@gmail.com>
2026-01-26 15:49:55 +0530

    Final Railway production config

--- soloRising/settings.py
+++ soloRising/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,8 @@ USE_I18N = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles" ##add neww
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"  ##add neww
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR/ 'media'
