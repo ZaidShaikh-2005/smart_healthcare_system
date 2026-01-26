2a45898cbabf4d0aed18f9849942422c8095ab64
D-FAKE <shaikhzaid23154@gmail.com>
2026-01-26 20:25:20 +0530

    Import symptoms into production DB

--- soloRising/settings.py
+++ soloRising/settings.py
@@ -158,11 +158,11 @@ DATABASES = {
 }### add new
 
 
-# ===== ONE TIME SYMPTOM IMPORT (REMOVE AFTER SUCCESS) =====
-if os.environ.get("RAILWAY_ENVIRONMENT") == "production":
-    try:
-        from django.core.management import call_command
-        call_command("import_symptoms")
-    except Exception as e:
-        print("IMPORT SYMPTOMS ERROR:", e)
-### add new
+# # ===== ONE TIME SYMPTOM IMPORT (REMOVE AFTER SUCCESS) =====
+# if os.environ.get("RAILWAY_ENVIRONMENT") == "production":
+#     try:
+#         from django.core.management import call_command
+#         call_command("import_symptoms")
+#     except Exception as e:
+#         print("IMPORT SYMPTOMS ERROR:", e)
+# ### add new
