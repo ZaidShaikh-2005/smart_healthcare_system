8d8572b68a82cd74d0053f01c695b7334791bfa4
D-FAKE <shaikhzaid23154@gmail.com>
2026-01-26 16:22:27 +0530

    Fix requirements and database config for production

--- soloRising/settings.py
+++ soloRising/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'soloRising.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -145,3 +145,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = [
     "https://web-production-4fbff.up.railway.app",
 ]  ##add new
+
+
+import os
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
+}
