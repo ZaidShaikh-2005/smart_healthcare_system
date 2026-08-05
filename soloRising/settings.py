fb5ddbbb3d7ff17dfbe7e0fe8ef3964b0f587a4c
D-FAKE <shaikhzaid23154@gmail.com>
2026-08-05 23:04:28 +0530

    Fix Python environment and Django compatibility

--- soloRising/settings.py
+++ soloRising/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-^_so9^44k_^dr_-3r(w5^&-v=f44df3h0c(v4nrlg91dvw4u-h"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = "True"
 
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
