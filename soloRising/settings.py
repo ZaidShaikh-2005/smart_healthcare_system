2a6b4fc23c728e5fbdbdb426e8372f877a39c0ce
D-FAKE <shaikhzaid23154@gmail.com>
2026-08-05 20:39:33 +0530

    Prepare project for Render deployment

--- soloRising/settings.py
+++ soloRising/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^_so9^44k_^dr_-3r(w5^&-v=f44df3h0c(v4nrlg91dvw4u-h'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*'] #'192.168.0.109', 'localhost', '127.0.0.1'
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",") #'192.168.0.109', 'localhost', '127.0.0.1'
 
 # Application definition
 
@@ -143,9 +146,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://web-production-4fbff.up.railway.app",
-]  ##add new
-
+    "https://*.onrender.com",
+]
 
 import dj_database_url
 
