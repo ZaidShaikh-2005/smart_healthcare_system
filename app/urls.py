e4b1764bd27ca4884ad5d2ece84fc1ec1b7679f7
D-FAKE <shaikhzaid23154@gmail.com>
2026-08-05 23:11:14 +0530

    Fix deployment and Python environment

--- app/urls.py
+++ app/urls.py
@@ -202,8 +202,7 @@ urlpatterns = [
 # =====================================================
 # MEDIA FILES (DEV ONLY)
 # =====================================================
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
--- soloRising/settings.py
+++ soloRising/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "True"
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
