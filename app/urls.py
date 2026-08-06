03844e4ef62996f9fa45e42c89f09fdc1d09d7be
D-FAKE <shaikhzaid23154@gmail.com>
2026-08-06 19:18:44 +0530

    Configure production settings

--- app/urls.py
+++ app/urls.py
@@ -202,7 +202,8 @@ urlpatterns = [
 # =====================================================
 # MEDIA FILES (DEV ONLY)
 # =====================================================
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
--- soloRising/settings.py
+++ soloRising/settings.py
@@ -26,12 +26,17 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+# DEBUG = "True"
 
+# ALLOWED_HOSTS = os.environ.get(
+#     "ALLOWED_HOSTS",
+#     "localhost,127.0.0.1"
+# ).split(",") #'192.168.0.109', 'localhost', '127.0.0.1'
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
-    "localhost,127.0.0.1"
-).split(",") #'192.168.0.109', 'localhost', '127.0.0.1'
+    "localhost,127.0.0.1,smart-healthcare-system-vull.onrender.com"
+).split(",")
 
 # Application definition
 
@@ -80,14 +85,18 @@ WSGI_APPLICATION = 'soloRising.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
     )
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
